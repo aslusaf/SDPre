e993a03c624f4a4898c628b56ed39bcd4b2fcf08
aslusaf <aslusaf@live.com>
2022-01-13 14:52:18 -0700

    Objective2Lab2 Completed

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Austin L";
+    int age = 30;
+    String state = "Tennesee";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
