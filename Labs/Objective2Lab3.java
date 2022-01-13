fd3845ae89fe982550e5c503db9df9cf911421ee
aslusaf <aslusaf@live.com>
2022-01-13 15:10:52 -0700

    Objective2Lab3 Completed

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,19 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+
+    System.out.println((num1 + num2 + num3) / 3);
+
+    /*
+      or
+
+      double avg = (num1 + num2 + num3) / 3;
+      System.out.println(avg);
+    */
+
+  }
+}
