b7c1d057e6a63caf071a2a803505fa02ac73c3f0
aslusaf <aslusaf@live.com>
2022-01-15 15:57:58 -0700

    Objective7Lab4.java Completed

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+
+    int total = 0, counter = 1;
+
+    while (counter <= 20) {
+      total += counter;
+      counter++;
+    }
+
+    System.out.println(total);
+  }
+}
